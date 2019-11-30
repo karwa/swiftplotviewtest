9e4439b37815c61ae8ab0aee96d94b0139c24e97
Karl Wagner <razielim@gmail.com>
2019-11-30 19:54:44 +0100

    Add UIKit support

--- Sources/SwiftPlotViews/PlotViews.swift
+++ Sources/SwiftPlotViews/PlotViews.swift
@@ -1,8 +1,9 @@
 import SwiftPlot
 import QuartzRenderer
+
+// SwiftPlot - AppKit bridge.
 #if canImport(AppKit)
 import AppKit
-
 public class PlotNSView: NSView {
     public var plot: Plot? {
         didSet { setNeedsDisplay(bounds) }
@@ -16,11 +17,31 @@ public class PlotNSView: NSView {
 }
 #endif
 
+// SwiftPlot - UIKit bridge.
+#if canImport(UIKit)
+public class PlotUIView: UIView {
+    public var plot: Plot? {
+        didSet { setNeedsDisplay(bounds) }
+    }
+    public override func draw(_ rect: CGRect) {
+        guard let plot = plot else { return }
+        let size = Size(bounds.size)
+        let ctx = UIGraphicsGetCurrentContext()!
+        let transform = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: CGFloat(size.height))
+        ctx.concatenate(transform)
+        let renderer = QuartzRenderer(externalContext: ctx, dimensions: size)
+        plot.drawGraph(size: size, renderer: renderer)
+    }
+}
+#endif
+
+// SwiftPlot - SwiftUI bridge.
 #if canImport(SwiftUI)
 import SwiftUI
 
-public struct PlotView: View {
-    // TODO: Is there some way to replace this with a @State variable?
+public struct PlotView {
+    // TODO: Is there a cleaner way to do this?
+    // We want setting a new Plot to invalidate the view.
     private class Box: ObservableObject {
         var plot: Plot? {
             willSet { self.objectWillChange.send() }
@@ -51,6 +72,20 @@ extension PlotView: NSViewRepresentable {
     }
     public func updateNSView(_ nsView: PlotNSView, context: Context) {
         nsView.plot = box.plot
+    }
+}
+#endif
+
+// SwiftUI - UIKit bridge.
+#if canImport(UIKit)
+extension PlotView: UIViewRepresentable {
+    public func makeUIView(context: Context) -> PlotUIView {
+        let view = PlotUIView()
+        view.plot = box.plot
+        return view
+    }
+    public func updateUIView(_ uiView: PlotUIView, context: Context) {
+        uiView.plot = box.plot
     }
 }
 #endif
