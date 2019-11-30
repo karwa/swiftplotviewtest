2ec18d34ec2dfcbd3b22d9e744f89b2739971546
Karl Wagner <razielim@gmail.com>
2019-11-30 18:19:11 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPlotViewTest",
-    platforms: [.macOS("10.15"), .iOS("13"), .tvOS("13"), .watchOS("13")],
+    platforms: [.macOS("10.15"), .iOS("13.0"), .tvOS("13.0"), .watchOS("13.0")],
     products: [
       .executable(name: "SwiftPlotViewTest", targets: ["SwiftPlotViewTest"])
     ],
